c62d951c9d29c371d0be71172baa6f3d318e70b9
dross <andre04072000@gmail.com>
2019-04-28 21:11:48 +0700

    add somthing in huffman

--- huffman/main.c
+++ huffman/main.c
@@ -9,39 +9,64 @@ enum mode {
 };
 
 typedef struct pair_t {
-	char symbol;
-	char *code;
+	uchar symbol;
+	uchar *code;
 }pair_t;
 
 void error(char * err_msg, int err_code);
-node_t * make_node_arr(int * frequency_table, int number_of_unique_symblols, const int ALPHABET_SIZE);
+node_t * make_node_arr(int * frequency_table, int number_of_unique_symbols, const int ALPHABET_SIZE);
 int change_frequency(int * frequency_table, uchar * buffer, int size, const int ALPHABET_SIZE);
 void print_node(node_t * root);
 node_t * connect_node(node_t * node1, node_t * node2);
-void make_code_table(pair_t * code_table, node_t * root, char *code, int pos, int * i);
-char * get_symbol_code(pair_t * code_table, int code_table_size, char symbol);
+void make_code_table(pair_t * code_table, node_t * root, uchar *code, int pos, int * i);
+uchar * get_symbol_code(const pair_t * code_table, const size_t code_table_size, const uchar symbol);
 int power(int a, int n);
-char convert_byte_to_char(char * byte, const int BYTE_SIZE);
-void insert_in_str(char * dest, char * source, int count, int * pos);
-uchar * coding(pair_t * code_table, int code_table_size, char * res_str, char * buffer, int size);
-void mk(pair_t * code_table, int code_table_size, const int BUFFER_SIZE, FILE * in);
+uchar convert_binary_to_int(const uchar * byte);
+int insert_in_str(uchar * dest, uchar * source, const int count, const int pos);
+int coding(const pair_t * code_table, const size_t code_table_size, uchar * res_str, const uchar * buffer, const int size, 
+			uchar * byte, const int b_index, int * res_str_index);
+void make_compression(const pair_t * code_table, const size_t code_table_size, FILE * in, const int file_size);
+void save_file_info(const pair_t * code_table, const size_t code_table_size, const int file_size, FILE * out);
+void decompression(FILE * in);
+void compression(FILE * in);
+void get_code_table_from_file(uchar * code_table, const code_table_size, FILE * in);
+void make_byte_str(uchar * byte_str, uchar * buffer, const int size);
+int decoding(uchar * code_table, uchar * buffer, const int size, const int file_size, int real_file_size, FILE * out);
+uchar * dec_to_binary(uchar symbol);
 
 
 int main(){
-	const int BUFFER_SIZE = 1000;
-	const int ALPHABET_SIZE =  256;
-	FILE * in = fopen("in.txt", "r");
+	FILE * in = fopen("out.txt", "rb");
 	if (in == NULL) {
 		error("Cant open input file", 0);
 	}
 	char mode = 0;
-	fscanf(in, "%c\n", &mode);
+	//fscanf(in, "%c\n", &mode);
+	//compression(in);
+	decompression(in);
+	/*if (mode == COMPRESSION){
+		compression(in);
+	}
+	else if (mode == DECOMPRESSION){
+		decompression(in);
+	}
+	else{
+		error("Wrong mode", 3);
+	}*/
+	return 0;
+}
+
+void compression(FILE * in){
+	const int BUFFER_SIZE = 1000;
+	const int ALPHABET_SIZE =  256;
 	uchar * buffer = (uchar *)calloc(BUFFER_SIZE, sizeof(uchar));
 	int * frequency_table = (int *)calloc(ALPHABET_SIZE, sizeof(int));
 	int size = 0;
 	int number_of_unique_symbols = 0;
+	int file_size = 0;
 	while ((size = fread(buffer, sizeof(char), BUFFER_SIZE, in))) {
 		number_of_unique_symbols += change_frequency(frequency_table, buffer, size, ALPHABET_SIZE);
+		file_size += size;
 	}
 	free(buffer);
 	node_t * arr = make_node_arr(frequency_table, number_of_unique_symbols, ALPHABET_SIZE);
@@ -56,70 +81,196 @@ int main(){
 	free(frequency_table);
 	pair_t * code_table = (pair_t *)malloc(sizeof(pair_t) * number_of_unique_symbols);
 	node_t tree = extract(heap);
-	char c[100] = { 0 };
+	uchar c[100] = { 0 };
 	int code_index = 0;
 	make_code_table(code_table, &tree, c, 0, &code_index);
-	for (int i = 0; i < number_of_unique_symbols; ++i) {
-		printf("%c - %s\n", code_table[i].symbol, code_table[i].code);
+	for (int i = 0; i < number_of_unique_symbols; ++i){
+		printf("sym %c - code %s\n", code_table[i].symbol, code_table[i].code);
 	}
-	return 0;
+	make_compression(code_table, number_of_unique_symbols, in, file_size);
 }
 
-void mk(pair_t * code_table, int code_table_size, const int BUFFER_SIZE, FILE * in) {
+void decompression(FILE * in){
+	const int ALPHABET_SIZE =  256;
+	const int BUFFER_SIZE = 100;
+	int file_size = 0;
+	int code_table_size = 0;
+	fread(&file_size, sizeof(int), 1, in);
+	fread(&code_table_size, sizeof(int), 1, in);
+	printf("%d %d\n", file_size, code_table_size);
+	pair_t * code_table = (pair_t *)malloc(sizeof(par_t) * code_table_size);
+	get_code_table_from_file(code_table, code_table_size, in);
+	FILE * out = fopen("dec_out.txt", "w");
+	if (out == NULL){
+		error("Cant open output file", 3);
+	}
+	uchar * buffer = (uchar *)calloc(BUFFER_SIZE + 1, sizeof(uchar));
+	int size = 0;
+	int real_file_size = 0;
+	while ((size = fread(buffer, sizeof(uchar), BUFFER_SIZE, in))){
+		real_file_size = decoding(code_table, buffer, size, file_size, real_file_size, out);
+	}
+}
+
+int decoding(uchar * code_table, uchar * buffer, const int size, const int file_size, int real_file_size, FILE * out){
+	int byte_str_size = size * 8;
+	uchar * byte_str = (uchar *)malloc(sizeof(uchar) * (byte_str_size + 1));
+	uchar * res_str = (uchar *)malloc(sizeof(uchar) * (file_size + 1));
+	int res_str_index = 0;
+	make_byte_str(byte_str, buffer, size);
+	printf("byte = %s\n", byte_str);
+	uchar code_str[9] = {0};
+	int code_index = 0;
+	int code = 0;
+	for (int i = 0; i < byte_str_size && real_file_size < file_size; ++i){
+		code_str[code_index++] = byte_str[i];
+		code = convert_binary_to_int(code_str);
+		if (code_table[code]){
+			res_str[res_str_index++] = code_table[code];
+			real_file_size++;
+			memset(code_str, 0, sizeof(uchar) * 9);
+			code_index = 0;
+		}
+	}
+	fwrite(res_str, sizeof(uchar), res_str_index, out);
+	return real_file_size;
+}
+
+void make_byte_str(uchar * byte_str, uchar * buffer, const int size){
+	int pos = 0;
+	uchar * byte = 0;
+	for (int i = 0; i < size; ++i){
+		byte = dec_to_binary(buffer[i]);
+		pos = insert_in_str(byte_str, byte, 8, pos);
+		free(byte);
+	}
+}
+
+uchar * dec_to_binary(uchar symbol){
+	const int BYTE_SIZE = 8;
+	uchar * byte = (uchar *)calloc(BYTE_SIZE + 1, sizeof(uchar));
+	int i = 0;
+	while (i < 8){
+		byte[i++] = (symbol % 2) + '0';
+		symbol /= 2;
+	}
+	reverse(byte, BYTE_SIZE);
+	return byte;
+}
+
+void reverse(uchar * str, const int size){
+	uchar tmp = 0;
+	for (int i = 0; i < size / 2; ++i){
+		tmp = str[i];
+		str[i] = str[size - i - 1];
+		str[size - i - 1] = tmp;
+	}
+}
+
+void get_code_table_from_file(pair_t * code_table, const code_table_size, FILE * in) {
+	uchar symbol = 0;
+	int code = 0;
+	for (int i = 0; i < code_table_size; ++i){
+		fread(&symbol, sizeof(uchar), 1, in);
+		fread(&code, sizeof(uchar), 1, in);
+		code_table[code] = symbol;
+	}
+}
+
+void make_compression(const pair_t * code_table, const size_t code_table_size, FILE * in, const int file_size) {
+	const int BYTE_SIZE = 8;
+	const int BUFFER_SIZE = 1000;
 	fseek(in, 2, SEEK_SET);
 	uchar * res_str = (uchar *)calloc(BUFFER_SIZE + 1, sizeof(uchar));
-	uchar * buffer = (uchar *)malloc((BUFFER_SIZE + 1) * sizeof(uchar));
+	uchar * buffer = (uchar *)calloc(BUFFER_SIZE + 1, sizeof(uchar));
+	uchar * byte = (uchar *)calloc(BYTE_SIZE + 1, sizeof(uchar));
+	FILE * out = fopen("out.txt", "wb");
+	if (out == NULL){
+		error("Cant open output file", 1);
+	}
+	save_file_info(code_table, code_table_size, file_size, out);
+	int byte_index = 0;
 	int size = 0;
+	int res_str_index = 0;
 	while ((size = fread(buffer, sizeof(uchar), BUFFER_SIZE, in))) {
-		coding(code_table, code_table_size, res_str, buffer, size);
+		byte_index = coding(code_table, code_table_size, res_str, buffer, size, byte, byte_index, &res_str_index);
+		fwrite(res_str, sizeof(uchar), res_str_index, out);
+		if (size < BUFFER_SIZE){
+			byte_fill(byte, byte_index, BYTE_SIZE);
+			uchar sym = convert_binary_to_int(byte);
+			fwrite(&sym, sizeof(uchar), 1, out);
+		}
+		memset(res_str, 0, sizeof(uchar) * res_str_index);		
+	}
+	fclose(out);
+}
+
+void save_file_info(const pair_t * code_table, const size_t code_table_size, const int file_size, FILE * out){
+	fwrite(&file_size, sizeof(int), 1, out);
+	fwrite(&code_table_size, sizeof(int), 1, out);
+	uchar code = 0;
+	for (int i = 0; i < code_table_size; ++i){
+		fwrite(&(code_table[i].symbol), sizeof(uchar), 1, out);
+		code = convert_binary_to_int(code_table[i].code);	
+		fwrite(&code, sizeof(uchar), 1, out);
+		//fwrite(&code_table[i].code, sizeof(uchar), strlen(code_table[i].code), out);
+	}
+}
+
+void byte_fill(uchar * byte, const int byte_index, const int BYTE_SIZE){
+	int b_ind = byte_index;
+	for (b_ind; b_ind < BYTE_SIZE; ++b_ind){
+		byte[b_ind] = '0';
 	}
 }
 
 
-uchar * coding(pair_t * code_table, int code_table_size, char * res_str, char * buffer, int size) {
+int coding(const pair_t * code_table, const size_t code_table_size, uchar * res_str, const uchar * buffer, const int size, 
+			uchar * byte, const int b_index, int * res_str_index) {
 	const int BYTE_SIZE = 8;
-	char * byte = (char *)malloc((BYTE_SIZE + 1) * sizeof(char));
-	int i = 0;
-	char * symbol_code = 0;
+	int byte_index = b_index;
+	uchar * symbol_code = 0;
 	int code_size = 0;
-	int byte_index = 0;
 	char res_symbol = 0;
-	int res_str_index = 0;
-	for (i = 0; i < size; ++i) {
+	for (int i = 0; i < size; ++i) {
 		symbol_code = get_symbol_code(code_table, code_table_size, buffer[i]);
 		code_size = strlen(symbol_code);
-		if (code_size + byte_index < BYTE_SIZE) {
-			insert_in_str(byte, symbol_code, code_size, &byte_index);
+		//printf("code = %s\n", symbol_code);
+		if (code_size + byte_index <= BYTE_SIZE) { 																// maybe <
+			byte_index = insert_in_str(byte, symbol_code, code_size, byte_index);
 		}
 		else {
-			insert_in_str(byte, symbol_code, BYTE_SIZE - byte_index - 1, &byte_index);
-			res_symbol = convert_byte_to_char(byte, BYTE_SIZE);
-			res_str[res_str_index++] = res_symbol;
+			int count_to_save = code_size - (BYTE_SIZE - byte_index);
+			byte_index = insert_in_str(byte, symbol_code, BYTE_SIZE - byte_index, byte_index);
+			res_symbol = convert_binary_to_int(byte);
+			//printf("%s == %c\n", byte, res_symbol);
+			res_str[(*res_str_index)++] = res_symbol;
 			memset(byte, 0, sizeof(char) * BYTE_SIZE);
-			int j = 0;
-			//for (j = BYTE_SIZE ; j <)
-			byte_index = 0;
+			for (byte_index = 0; byte_index < count_to_save; ++byte_index){
+				byte[byte_index] = symbol_code[code_size - count_to_save + byte_index];
+			}
 		}
-
 	}
-
+	return byte_index;
 }
 
-void insert_in_str(char * dest, char * source, int count, int * pos) {
+int insert_in_str(uchar * dest, uchar * source, const int count, const int pos) {
 	int i = 0;
-	while (count) {
-		dest[*pos] = source[i++];
-		*pos += 1;
-		count--;
+	int position = pos;
+	while (i < count) {
+		dest[position] = source[i++];
+		position++;
 	}
+	return position;
 }
 
-char convert_byte_to_char(char * byte, const int BYTE_SIZE) {
-	char symbol = 0;
+uchar convert_binary_to_int(const uchar * byte) {
+	uchar symbol = 0;
 	int i = 0;
-	for (i = 0; i < BYTE_SIZE; ++i) {
+	int size = strlen(byte);
+	for (i = 0; i < size; ++i) {
 		if (byte[i] == '1') {
-			symbol += power(2, BYTE_SIZE - 1 - i);
+			symbol += power(2, size - 1 - i);
 		}
 	}
 	return symbol;
@@ -136,7 +287,7 @@ int power(int a, int n) {
 }
 
 
-char * get_symbol_code(pair_t * code_table, int code_table_size, char symbol) {
+uchar * get_symbol_code(const pair_t * code_table, const size_t code_table_size, const uchar symbol) {
 	int i = 0;
 	for (i = 0; i < code_table_size; ++i) {
 		if (code_table[i].symbol == symbol) {
@@ -145,7 +296,7 @@ char * get_symbol_code(pair_t * code_table, int code_table_size, char symbol) {
 	}
 }
 
-void make_code_table(pair_t * code_table, node_t * root, char *code, int pos, int * code_index) {
+void make_code_table(pair_t * code_table, node_t * root, uchar *code, int pos, int * code_index) {
 	if (root) {
 		if (!root->right && !root->left) {
 			pair_t pair;
@@ -156,26 +307,16 @@ void make_code_table(pair_t * code_table, node_t * root, char *code, int pos, in
 			*code_index += 1;
 		}
 		if (root->right) {
-			code[pos] = '1';
+			code[pos] = '0';
 			make_code_table(code_table, root->right, code, pos+1, code_index);
 		}
 		if (root->left) {
-			code[pos] = '0';
+			code[pos] = '1';
 			make_code_table(code_table, root->left, code, pos + 1, code_index);
 		}
 	}
 }
 
-void print_node(node_t * root) {
-	if (root == NULL) {
-		return;
-	}
-	if (root->symbol) {
-		printf("%c\n", root->symbol);
-	}
-	print_node(root->right);
-	print_node(root->left);
-}
 
 node_t * connect_node(node_t * node1, node_t * node2) {
 	node_t * res_node = (node_t *)malloc(sizeof(node_t));
@@ -190,8 +331,8 @@ node_t * connect_node(node_t * node1, node_t * node2) {
 	return res_node;
 }
 
-node_t * make_node_arr(int * frequency_table, int number_of_unique_symblols, const int ALPHABET_SIZE) {
-	node_t * arr = (node_t *)malloc(sizeof(node_t) * number_of_unique_symblols);
+node_t * make_node_arr(int * frequency_table, int number_of_unique_symbols, const int ALPHABET_SIZE) {
+	node_t * arr = (node_t *)malloc(sizeof(node_t) * number_of_unique_symbols);
 	int i = 0;
 	int j = 0;
 	node_t node;
